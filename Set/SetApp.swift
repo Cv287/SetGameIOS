e1ef66a1091a1b6799a2e73f0fb26545089a8309
Cv287 <muchi17386@gmail.com>
2021-07-15 15:18:31 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Олег Куколев on 15.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
